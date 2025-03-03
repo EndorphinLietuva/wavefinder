b6e54766b8522852c84d7c2438cef07ffc4ddd27
Donatas Šauklys <donatas.sauklys@gmail.com>
2025-03-03 21:45:55 +0200

    Add initial layout components including Navbar and Footer, and update Home page design

---
+++ resources/js/Components/Navbar.jsx
@@ -0,0 +1,82 @@
+import { Link } from "@inertiajs/react";
+
+export default function Navbar() {
+	return (
+		<div>
+			<div className="navbar bg-base-100 shadow-sm">
+				<div className="navbar-start">
+					<div className="dropdown">
+						<div
+							tabIndex={0}
+							role="button"
+							className="btn btn-ghost lg:hidden">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								className="h-5 w-5"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke="currentColor">
+								{" "}
+								<path
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									strokeWidth="2"
+									d="M4 6h16M4 12h8m-8 6h16"
+								/>{" "}
+							</svg>
+						</div>
+						<ul
+							tabIndex={0}
+							className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+							<li>
+								<Link>Browse</Link>
+							</li>
+							<li>
+								<Link>Random Station</Link>
+							</li>
+							<li>
+								<Link>TBA</Link>
+								<ul className="p-2">
+									<li>
+										<Link>Submenu 1</Link>
+									</li>
+									<li>
+										<Link>Submenu 2</Link>
+									</li>
+								</ul>
+							</li>
+						</ul>
+					</div>
+					<Link className="btn btn-ghost text-xl">Wavefinder</Link>
+				</div>
+				<div className="navbar-center hidden lg:flex">
+					<ul className="menu menu-horizontal px-1">
+						<li>
+							<Link>Browse</Link>
+						</li>
+						<li>
+							<Link>Random Station</Link>
+						</li>
+						<li>
+							<details>
+								<summary>TBA</summary>
+								<ul className="p-2">
+									<li>
+										<Link>Submenu 1</Link>
+									</li>
+									<li>
+										<Link>Submenu 2</Link>
+									</li>
+								</ul>
+							</details>
+						</li>
+					</ul>
+				</div>
+				<div className="navbar-end">
+					<Link className="btn">Log in</Link>
+					<Link className="btn btn-primary">Register</Link>
+				</div>
+			</div>
+		</div>
+	);
+}
--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,7 +1,18 @@
 export default function Home() {
-    return <>
-        <h1>Home Test</h1>
-        <button className="btn">Button</button>
-        <button className="btn btn-primary">Primary</button>
-    </>
+	return (
+		<div className="relative flex items-center justify-center overflow-hidden">
+			<div className="fixed -z-10 top-0 left-1/2 transform -translate-x-1/3 -translate-y-1/2 flex items-center justify-center">
+				<div className="bg-primary w-[1000px] h-[1000px] rounded-full blur-3xl opacity-10"></div>
+			</div>
+			<div className="relative z-10 mt-20">
+				<h1 className="text-center text-4xl font-bold">
+					Home Test Text
+				</h1>
+				<div className="flex flex-row justify-center mt-4">
+					<button className="btn">Button</button>
+					<button className="btn btn-primary ml-4">Primary</button>
+				</div>
+			</div>
+		</div>
+	);
 }
